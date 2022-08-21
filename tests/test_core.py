d7804e27b288e8ce53180e3a024e9045b2a48bb3
estripling <estripling042@gmail.com>
2022-08-21 18:28:38 +0200

    feat(core.py): add ackley

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,6 +6,20 @@ from fbench import core
 @pytest.mark.parametrize(
     "arg, expected",
     [
+        ([0], 0.0),
+        ([0, 0], 0.0),
+        ([1, 1], 3.6254),
+        ([2, 2], 6.5936),
+    ],
+)
+def test_ackley(arg, expected):
+    actual = core.ackley(x=arg)
+    assert round(actual, 4) == expected
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
         ([0], 0),
         ([0, 0], 0),
         ([1, 1], 2),
