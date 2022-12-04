046bece0835cfc19b918453339ca73c88640554c
estripling <estripling042@gmail.com>
2022-12-04 19:05:49 +0100

    feat(core.py): add rosenbrock

--- tests/test_core.py
+++ tests/test_core.py
@@ -39,6 +39,27 @@ def test_rastrigin(arg, expected):
 @pytest.mark.parametrize(
     "arg, expected",
     [
+        ([0], 0.0),
+        ([0, 0], 1.0),
+        ([1, 1], 0.0),
+        ([1, 1, 1], 0.0),
+        ([1, 1, 1, 1], 0.0),
+        ([1, 2], 100.0),
+        ([2, 1], 901.0),
+        ([2, 2], 401.0),
+        ([4, 4.5], 13234.0),
+        ([4.5, 4], 26418.5),
+        ([5.12, 5.12], 44514.35),
+    ],
+)
+def test_rosenbrock(arg, expected):
+    actual = fb.rosenbrock(x=arg)
+    assert round(actual, 2) == expected
+
+
+@pytest.mark.parametrize(
+    "arg, expected",
+    [
         ([0], 0),
         ([0, 0], 0),
         ([1, 1], 2),
