ec5b32459ad26b3a82dfa5edebe571e5531d174c
estripling <estripling042@gmail.com>
2023-02-12 10:53:42 +0000

    test(cleanup.py): remove prefix

--- scripts/cleanup.py
+++ scripts/cleanup.py
@@ -10,14 +10,14 @@ def main():
     directories = ["__pycache__", ".pytest_cache", ".ipynb_checkpoints"]
 
     for file_extension in file_extensions:
-        for pathlib.path in cwd.rglob(file_extension):
-            print(f"deleting {pathlib.path}")
-            pathlib.path.unlink()
+        for path in cwd.rglob(file_extension):
+            print(f"deleting {path}")
+            path.unlink()
 
     for directory in directories:
-        for pathlib.path in cwd.rglob(directory):
-            print(f"deleting {pathlib.path}")
-            shutil.rmtree(pathlib.path.absolute(), ignore_errors=False)
+        for path in cwd.rglob(directory):
+            print(f"deleting {path}")
+            shutil.rmtree(path.absolute(), ignore_errors=False)
 
 
 if __name__ == "__main__":
